21339a2baeae550714b8c83a55c552948b6fc60e
HSGill <harjotgill73@gmail.com>
2020-02-14 19:53:03 +1300

    first commit

---
+++ TagChecker.js
@@ -0,0 +1,56 @@
+/**
+ * @param {string} code
+ * @return {boolean}
+ */
+var isValid = function (code) {
+    let stack = [];
+
+    for (let i = 0; i < code.length; i++) {
+        if (code.charAt(i) == '<' && (code.charAt(i + 1) != '/')) {
+            let closeIndex = code.indexOf('>', i + 1)
+            if (closeIndex < 0) {
+                return false;
+            }
+            else {
+                let str = code.substring(i + 1, closeIndex)
+                if (str == str.toUpperCase() && (str != '*' || '<')) {
+
+                    stack.push(str);
+                }
+
+            }
+        }
+        else {
+            if (code.charAt(i + 1) == '/') {
+
+                let closeIndex = code.indexOf('>', i + 1)
+                if (closeIndex < 0) {
+                    return false;
+                }
+                else {
+                    let str = code.substring(i + 2, closeIndex)
+
+                    if (stack.length != 0 || stack.length - 1 == str) {
+                        stack.pop();
+                    }
+                    else {
+                        if (str == str.toUpperCase() && (str != '*' || '<')) {
+
+                            stack.push(str);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    if (stack.length == 0) {
+        return "Correctly tagged paragraph";
+    }
+    else {
+        return stack;
+    }
+
+};
+
+console.log(isValid('<B><C>This should be centred and in boldface, but there isa missing closing tag</C>'));
