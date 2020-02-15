446ce2e05a950e3b3de3086f99fd68629243604b
HSGill <harjotgill73@gmail.com>
2020-02-15 17:02:44 +1300

    final commit

--- TagChecker.js
+++ TagChecker.js
@@ -13,11 +13,10 @@ var isValid = function (code) {
             }
             else {
                 let str = code.substring(i + 1, closeIndex)
-                if (str == str.toUpperCase() && (str != '*' || '<')) {
+                if (str == str.toUpperCase() && (str != '*' && str != '<*' && str != '' && str != '<')) {
 
                     stack.push(str);
                 }
-
             }
         }
         else {
@@ -30,13 +29,13 @@ var isValid = function (code) {
                 else {
                     let str = code.substring(i + 2, closeIndex)
 
-                    if (stack.length != 0 || stack.length - 1 == str) {
+                    if (stack.length != 0 && stack[stack.length - 1] === str) {
                         stack.pop();
                     }
                     else {
                         if (str == str.toUpperCase() && (str != '*' || '<')) {
-
-                            stack.push(str);
+                            if (stack.length == 0)return "Expected #" + " found </" + str + ">";
+                            return "Expected <" + stack[0] + ">" + " found </" + str + ">";
                         }
 
                     }
@@ -48,7 +47,12 @@ var isValid = function (code) {
         return "Correctly tagged paragraph";
     }
     else {
-        return stack;
+        return "Expected </" + stack[0] + ">" + " found #";
     }
 
 };
+console.log(isValid('The following text<C><B>is centred and in boldface</B></C>'));
+console.log(isValid('<B>This <\g>is <B>boldface</B> in <<*> a</B> <\6> <<d>sentence'));
+console.log(isValid('<B><C> This should be centred and in boldface, but the tags are wrongly nested </B></C>'));
+console.log(isValid('<B>This should be in boldface, but there is an extra closing tag</B></C>'));
+console.log(isValid('<B><C>This should be centred and in boldface, but there is a missing closing tag</C>'));
